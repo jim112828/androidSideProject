f0e054751cf7c97284700337d7aed7c01148420b
Ting Chih Hsu <hsujimmy@google.com>
2022-09-29 17:27:00 +0800

    add musicplayer

---
+++ musicplayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer music;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        music = MediaPlayer.create(this, R.raw.musicTest);
+    }
+
+    public void  musicPlay(View view){
+        music.start();
+    }
+
+    public void  musicPause(View view){
+        music.pause();
+    }
+
+    public void  musicStop(View view){
+        music.stop();
+        music = MediaPlayer.create(this,R.raw.musicTest);
+    }
+
+}
